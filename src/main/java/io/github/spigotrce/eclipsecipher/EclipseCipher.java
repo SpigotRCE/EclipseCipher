94cfeaa86008bd156663ea6c576c6aa9a37e433e
SpigotRCE <spigotrce@gmail.com>
2024-11-29 20:07:47 +0530

    feat: helper methods

--- src/main/java/io/github/spigotrce/eclipsecipher/EclipseCipher.java
+++ src/main/java/io/github/spigotrce/eclipsecipher/EclipseCipher.java
@@ -1,4 +1,35 @@
 package io.github.spigotrce.eclipsecipher;
 
+import java.security.SecureRandom;
+
 public class EclipseCipher {
+    public static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    public static String generateRandomString() {
+        SecureRandom secureRandom = new SecureRandom();
+        StringBuilder stringBuilder = new StringBuilder(2048);
+
+        for (int i = 0; i < 2048; i++) {
+            int randomIndex = secureRandom.nextInt(EclipseCipher.CHARACTERS.length());
+            stringBuilder.append(EclipseCipher.CHARACTERS.charAt(randomIndex));
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public static String deserialize(long i) {
+        StringBuilder r = new StringBuilder();
+        while (i > 0) {
+            r.append(CHARACTERS.charAt((int) (i % CHARACTERS.length())));
+            i /= CHARACTERS.length();
+        }
+        return r.reverse().toString();
+    }
+
+    public static long serialize(String t) {
+        long r = 0;
+        for (char c : t.toCharArray())
+            r = r * CHARACTERS.length() + CHARACTERS.indexOf(c);
+        return r;
+    }
 }
