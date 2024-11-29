9ec11ffe168a38122b1066b5ebc8830c9a64a36d
SpigotRCE <spigotrce@gmail.com>
2024-11-29 23:48:50 +0530

    feat: javadoc

--- src/main/java/io/github/spigotrce/eclipsecipher/EclipseCipher.java
+++ src/main/java/io/github/spigotrce/eclipsecipher/EclipseCipher.java
@@ -3,10 +3,18 @@ package io.github.spigotrce.eclipsecipher;
 import java.security.SecureRandom;
 import java.util.Random;
 
+/**
+ * This class provides various utility methods for working with the EclipseCipher.
+ */
 public class EclipseCipher {
     // Charset
     public static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/ ";
 
+    /**
+     * Generates a secure random key of length 2048 characters.
+     *
+     * @return The generated secure random key.
+     */
     public static String generateKey() {
         SecureRandom secureRandom = new SecureRandom(); // The randomizer used to generate the secret key
         StringBuilder stringBuilder = new StringBuilder(2048); // Create a new StringBuilder
@@ -19,6 +27,13 @@ public class EclipseCipher {
         return stringBuilder.toString();
     }
 
+    /**
+     * Generates a string of characters using a given seed and length.
+     *
+     * @param seed   The seed for the random number generator.
+     * @param length The length of the generated string.
+     * @return       The generated string of characters.
+     */
     public static String getCharacters(int seed, int length) {
         Random random = new Random(); // The randomizer used to generate the characters
         random.setSeed(seed); // Set the seed of the randomizer
@@ -33,6 +48,12 @@ public class EclipseCipher {
         return stringBuilder.toString();
     }
 
+    /**
+     * Deserializes an integer into a string using the CHARACTERS charset.
+     *
+     * @param i The integer to be deserialized.
+     * @return  The deserialized string.
+     */
     public static String deserialize(int i) {
         StringBuilder r = new StringBuilder();
         while (i > 0) {
@@ -42,6 +63,12 @@ public class EclipseCipher {
         return r.reverse().toString();
     }
 
+    /**
+     * Serializes a string into an integer using the CHARACTERS charset.
+     *
+     * @param t The string to be serialized.
+     * @return  The serialized integer.
+     */
     public static int serialize(String t) {
         int r = 0;
         for (char c : t.toCharArray())
@@ -49,6 +76,12 @@ public class EclipseCipher {
         return r;
     }
 
+    /**
+     * Creates a new CipherManager instance with the given key.
+     *
+     * @param key The key for the CipherManager.
+     * @return    The newly created CipherManager instance.
+     */
     public static CipherManager newCipherManager(long key) {
         return new CipherManager(key);
     }
