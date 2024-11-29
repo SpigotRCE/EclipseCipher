a46ce5f8d1d947a7bd48187bdeecb1cbc2253b6a
SpigotRCE <spigotrce@gmail.com>
2024-11-29 23:05:52 +0530

    feat: commenting

--- src/main/java/io/github/spigotrce/eclipsecipher/EclipseCipher.java
+++ src/main/java/io/github/spigotrce/eclipsecipher/EclipseCipher.java
@@ -4,30 +4,32 @@ import java.security.SecureRandom;
 import java.util.Random;
 
 public class EclipseCipher {
+    // Charset
     public static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/ ";
 
     public static String generateKey() {
-        SecureRandom secureRandom = new SecureRandom();
-        StringBuilder stringBuilder = new StringBuilder(2048);
+        SecureRandom secureRandom = new SecureRandom(); // The randomizer used to generate the secret key
+        StringBuilder stringBuilder = new StringBuilder(2048); // Create a new StringBuilder
 
-        for (int i = 0; i < 2048; i++) {
-            int randomIndex = secureRandom.nextInt(EclipseCipher.CHARACTERS.length());
-            stringBuilder.append(EclipseCipher.CHARACTERS.charAt(randomIndex));
-        }
+        for (int i = 0; i < 2048; i++) // Iterate for length of the key
+            // Append a new secure char
+            stringBuilder.append(EclipseCipher.CHARACTERS.charAt(secureRandom.nextInt(EclipseCipher.CHARACTERS.length())));
 
+        // Return the key
         return stringBuilder.toString();
     }
 
     public static String getCharacters(int seed, int length) {
-        Random random = new Random();
-        random.setSeed(seed);
-        StringBuilder stringBuilder = new StringBuilder(length);
+        Random random = new Random(); // The randomizer used to generate the characters
+        random.setSeed(seed); // Set the seed of the randomizer
 
-        for (int i = 0; i < length; i++) {
-            int randomIndex = random.nextInt(EclipseCipher.CHARACTERS.length());
-            stringBuilder.append(EclipseCipher.CHARACTERS.charAt(randomIndex));
-        }
+        StringBuilder stringBuilder = new StringBuilder(length); // Create a new StringBuilder
 
+        for (int i = 0; i < length; i++) // Iterate for length of the key
+            // Append a new char
+            stringBuilder.append(EclipseCipher.CHARACTERS.charAt(random.nextInt(EclipseCipher.CHARACTERS.length())));
+
+        // Return the string
         return stringBuilder.toString();
     }
 
